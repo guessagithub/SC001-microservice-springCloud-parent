c6d7fcf7dbfa7b240c154de90c56af50530aa0e3
xn088608 <xn088608@ZHE201906101159.xiaoniu.com>
2019-07-04 18:41:28 +0800

    新建

---
+++ microservice-simple-gateway-zuul/src/main/java/com/itmuch/cloud/gateway/zuul/ZuulApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.gateway.zuul;
+
+/**
+ * Hello world!
+ *
+ */
+public class ZuulApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
