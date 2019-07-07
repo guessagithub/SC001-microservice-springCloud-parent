c75e504e0f73018f8e0822e50c31ac9a1954b9a0
guessagithub <guessa@163.com>
2019-07-08 00:33:36 +0800

    新建

---
+++ microservice-simple-file-upload/src/main/java/com/itmuch/cloud/file/upload/FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.file.upload;
+
+/**
+ * Hello world!
+ *
+ */
+public class FileUploadApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
