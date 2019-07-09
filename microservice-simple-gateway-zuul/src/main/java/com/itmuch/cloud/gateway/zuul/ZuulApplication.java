337275dbccb39965a0478b275aca2ce855d0ffe2
guessagithub <guessa@163.com>
2019-07-10 00:42:42 +0800

    更新

--- microservice-simple-gateway-zuul/src/main/java/com/itmuch/cloud/gateway/zuul/ZuulApplication.java
+++ microservice-simple-gateway-zuul/src/main/java/com/itmuch/cloud/gateway/zuul/ZuulApplication.java
@@ -3,11 +3,20 @@ package com.itmuch.cloud.gateway.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.itmuch.cloud.gateway.zuul.filter.MyPreRequestLogFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulApplication 
 {
+	
+	@Bean
+	public MyPreRequestLogFilter myPreRequestFilter() {
+		return new MyPreRequestLogFilter();
+	}
+	
     public static void main( String[] args )
     {
         SpringApplication.run(ZuulApplication.class, args);
---
+++ microservice-simple-gateway-zuul/src/main/java/com/itmuch/cloud/gateway/zuul/filter/MyPreRequestLogFilter.java
@@ -0,0 +1,40 @@
+package com.itmuch.cloud.gateway.zuul.filter;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class MyPreRequestLogFilter extends ZuulFilter{
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyPreRequestLogFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext context = RequestContext.getCurrentContext();
+		HttpServletRequest request = context.getRequest();
+		logger.info(String.format("================ send 【%s】 request to 【%s】 .", request.getMethod(), request.getRequestURL().toString()));
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
