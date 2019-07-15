6f3eb8919688716ea160337d8805c66d3037714e
xn088608 <xn088608@ZHE201906101159.xiaoniu.com>
2019-07-15 18:29:05 +0800

    更新

--- microservice-simple-gateway-zuul/src/main/java/com/itmuch/cloud/gateway/zuul/ZuulApplication.java
+++ microservice-simple-gateway-zuul/src/main/java/com/itmuch/cloud/gateway/zuul/ZuulApplication.java
@@ -2,11 +2,13 @@ package com.itmuch.cloud.gateway.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import com.itmuch.cloud.gateway.zuul.filter.MyPreRequestLogFilter;
 import com.itmuch.cloud.gateway.zuul.filter.MyRouteFilter;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -27,4 +29,20 @@ public class ZuulApplication
     {
         SpringApplication.run(ZuulApplication.class, args);
     }
+
+	/**
+	 * 低版本直接启动即可使用 http://ip:port/hystrix.stream 查看监控信息 
+	 * 高版本需要添加本方法方可使用 http://ip:port/hystix.stream 查看监控信息
+	 * 
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
 }
